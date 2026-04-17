70f049056389e501459eda0b2692f120d9d3da5e
Gerson Portal <gportallinares21@gmail.com>
2026-04-16 19:43:19 -0500

    feat: creacion de recursos en terraform

---
+++ iac/api.tf
@@ -0,0 +1,11 @@
+resource "docker_container" "api" {
+  name  = "api-${terraform.workspace}"
+  image = "lab/api"
+
+  ports {
+    internal = "3000"
+    external = var.api_port[terraform.workspace]
+  }
+    
+}
+
---
+++ iac/web.tf
@@ -0,0 +1,11 @@
+resource "docker_container" "web" {
+  name  = "web-${terraform.workspace}"
+  image = "lab/web"
+
+  ports {
+    internal = "80"
+    external = var.web_port[terraform.workspace]
+  }
+    
+}
+
